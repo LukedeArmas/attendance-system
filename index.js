6f8e9920cdb24e76d3d3513def00e409f1e89167
Luke deArmas <luke.p.dearmas@vanderbilt.edu>
2022-02-16 17:36:37 -0500

    Commit 9 part 8

--- index.js
+++ index.js
@@ -72,9 +72,46 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash())
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://ajax.googleapis.com/",
+]
+const styleSrcUrls = [
+    "https://cdnjs.cloudflare.com/",
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+]
+const connectSrcUrls = [
+    "https://ka-f.fontawesome.com/",
+]
+const fontSrcUrls = [
+    "https://cdnjs.cloudflare.com/",
+    "https://ka-f.fontawesome.com/",
+    "https://fonts.gstatic.com/",
+]
+const imgSrcUrls = []
+
+
 app.use(
-    helmet({
-         contentSecurityPolicy: false 
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: ["'self'", "blob:", "data:", ...imgSrcUrls],
+            fontSrc: ["'self'", ...fontSrcUrls]
+        }
     })
 )
 
